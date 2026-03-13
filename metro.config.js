a09fbb5308e079484b3940b7d7e3f71fd5cbef79
Chadrack Kanza <chadrackkanza2015@gmail.com>
2026-03-13 18:40:45 +0100

    fix: resolve react async hook in metro

--- metro.config.js
+++ metro.config.js
@@ -7,11 +7,19 @@ const defaultResolveRequest = config.resolver?.resolveRequest;
 const isProduction = process.env.NODE_ENV === 'production';
 
 config.resolver = config.resolver || {};
+config.resolver.resolverMainFields = ['react-native', 'browser', 'main', 'module'];
 config.resolver.resolveRequest = (context, moduleName, platform, oldMetroModuleName) => {
   if (!isProduction && moduleName === 'expo-keep-awake') {
     return {
       type: 'sourceFile',
       filePath: path.resolve(__dirname, 'src/shims/expo-keep-awake.ts'),
+    };
+  }
+
+  if (moduleName === 'react-async-hook') {
+    return {
+      type: 'sourceFile',
+      filePath: path.resolve(__dirname, 'node_modules/react-async-hook/dist/index.js'),
     };
   }
 
